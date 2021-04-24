84335b862f438c041805cd51826067d76ce0ea48
Sirichai <drink@finnomena.com>
2021-04-25 03:13:47 +0700

    [ Add ] Part 2

---
+++ ATOMIZ_File_Pattern_Part_2/ViewController.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,17 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - User Interaction
+
+    // MARK: - Helpers
+
+    func setup() {
+    }
+}
